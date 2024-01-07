52f57494f917f59ed557fba2be4b82ce03568bc6
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2024-01-07 01:56:14 -0500

    Simplify code

--- src/main.zig
+++ src/main.zig
@@ -25,59 +25,54 @@ pub fn calculateShortestEditDistance(
     // do diagonal_lines_best_xs[max_depth - 1], and likewise if I want diagonal_lines_best_xs[1]
     // I would do diagonal_lines_best_xs[max_depth + 1].
 
-    // Called to create valid case for first move which is off the board
+    // Called to create valid case for first move which is off the board.
     diagonal_lines_best_xs[max_depth + 1] = 0;
+
+    // Our long-term objective is for whatever move we perform
+    // to move towards the bottom right of the board. Our short-term
+    // objective is for every move to move towards the current diagonal.
 
     for (0..max_depth + 1) |depth| {
         // -max_depth, -max_depth + 2 ... max_depth - 2, max_depth
         var diagonal: usize = max_depth - depth;
         while (diagonal <= max_depth + depth) : (diagonal += 2) {
-            if (diagonal == max_depth - depth) {
-                // "Most left" diagonal (bottom bound)
-                // We can only go down from here
+            const should_go_right =
+                // "Most right" diagonal (top bound); diagonal is to the
+                // right of current position, so we can only go right from here.
+                (diagonal == max_depth + depth or
+                // If we arrive here, we must pick one of two cases (left or right); comparators:
+                // == : left diagonal is closer to bottom right, so pick it (move right)
+                // <  : right diagonal has higher X (move down)
+                // >  : left diagonal has higher X (move right)
+                diagonal_lines_best_xs[diagonal - 1] >= diagonal_lines_best_xs[diagonal + 1]) and
+                // "Most left" diagonal (bottom bound); diagonal is below the
+                // current position, so we can only go down from here.
+                diagonal != max_depth - depth;
 
-                // Going down means we do not change our X, so we take the X of the diagonal to the right ("above")
-                // and store that as our current diagonal's X, showing that no change in X has occurred
-                diagonal_lines_best_xs[diagonal] = diagonal_lines_best_xs[diagonal + 1];
-            } else if (diagonal == max_depth + depth) {
-                // "Most right" diagonal (top bound)
-                // We can only go right from here
-
+            if (should_go_right) {
+                // Going right means that we're increasing X by one with our move originating
+                // from the left and heading to our current diagonal. Thus, we need to copy
+                // the diagonal to the left os us' X, which is diagonal - 1, and then add 1 to it.
                 diagonal_lines_best_xs[diagonal] = diagonal_lines_best_xs[diagonal - 1] + 1;
             } else {
-                // We're not at the most left or most right; we must pick one of two options
-
-                if (diagonal_lines_best_xs[diagonal - 1] == diagonal_lines_best_xs[diagonal + 1]) {
-                    // Two options have equal x; to pick the closest to the bottom right,
-                    // we pick the closest y to the bottom right, which is always the left diagonal
-
-                    // Move right from left
-                    diagonal_lines_best_xs[diagonal] = diagonal_lines_best_xs[diagonal - 1] + 1;
-                } else if (diagonal_lines_best_xs[diagonal - 1] < diagonal_lines_best_xs[diagonal + 1]) {
-                    // Right diagonal has greater X, although it may be the same distance to the bottom right
-                    // we prioritize X movement so we pick the right diagonal.
-
-                    // Move down from up
-
-                    diagonal_lines_best_xs[diagonal] = diagonal_lines_best_xs[diagonal + 1];
-                } else if (diagonal_lines_best_xs[diagonal - 1] > diagonal_lines_best_xs[diagonal + 1]) {
-                    // Left diagonal has greater X, although it may be same distance to the bottom right
-                    // we prioritize X movement so we pick the left diagonal.
-
-                    // Move right from left
-                    diagonal_lines_best_xs[diagonal] = diagonal_lines_best_xs[diagonal - 1] + 1;
-                }
+                // Going down means that we're keeping the X the same with our move originating
+                // from above and heading to our current diagonal. Thus, we need to copy the
+                // diagonal above us' X, which is diagonal + 1.
+                diagonal_lines_best_xs[diagonal] = diagonal_lines_best_xs[diagonal + 1];
             }
 
+            // Extract x and y from formula `diagonal = x - y`
             var x = diagonal_lines_best_xs[diagonal];
             var y = x + max_depth - diagonal;
 
+            // Follow any snakes
             while (x < a.len and y < b.len and a[x] == b[y]) {
                 x += 1;
                 y += 1;
                 diagonal_lines_best_xs[diagonal] = x;
             }
 
+            // Are we at the bottom right corner?
             if (x >= a.len and y >= b.len) return depth;
         }
     }
@@ -86,6 +81,29 @@ pub fn calculateShortestEditDistance(
 }
 
 pub fn main() !void {
-    var scratch_buf: [128]usize = undefined;
-    std.log.info("{d}", .{calculateShortestEditDistance(u8, "aaaa", "bbbb", &scratch_buf)});
+    var seed: u64 = undefined;
+    try std.os.getrandom(std.mem.asBytes(&seed));
+
+    var rng = std.rand.DefaultPrng.init(seed);
+
+    var scratch_buf: [4097]usize = undefined;
+
+    var str_a: [1024]u8 = undefined;
+    rng.random().bytes(&str_a);
+
+    var str_b: [1024]u8 = str_a;
+
+    for (&str_b) |*c| {
+        if (rng.random().boolean()) {
+            c.* = rng.random().int(u8);
+        }
+    }
+
+    var timer = try std.time.Timer.start();
+
+    const a = calculateShortestEditDistance(u8, &str_a, &str_b, &scratch_buf);
+    const a_time = timer.lap();
+
+    std.debug.print("{d}\n", .{a});
+    std.debug.print("{d}ns == {d}ms\n", .{ a_time, @as(f32, @floatFromInt(a_time)) / @as(f32, @floatFromInt(std.time.ns_per_ms)) });
 }
